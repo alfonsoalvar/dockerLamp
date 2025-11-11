aad311d1e7c1a8e9bb5181eea5d5a51ae55d3ae3
Alfonso Alvar <agonanse@gmail.com>
2025-11-11 22:32:42 +0100

    Rastrear solo los directorios sample dentro de sites

--- mariadb/initdb/01-remote-user.sh
+++ mariadb/initdb/01-remote-user.sh
@@ -2,12 +2,10 @@
 
 echo "Creando usuario remoto: ${MARIADB_REMOTE_USER}"
 
-# Usamos 'mysql' para ejecutar comandos.
-# El usuario root ya está autenticado a nivel de la inicialización.
 mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" <<EOSQL
     -- Creamos el usuario remoto usando una variable de entorno para la contraseña remota
     CREATE USER '${MARIADB_REMOTE_USER}'@'%' IDENTIFIED BY '${MARIADB_REMOTE_PASSWORD}';
-    GRANT ALL PRIVILEGES ON *.* TO 'usuario_remoto'@'%' WITH GRANT OPTION;
+    GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_REMOTE_USER}'@'%' WITH GRANT OPTION;
     FLUSH PRIVILEGES;
 
     -- Opcional: Permitir al root conectarse desde fuera también
